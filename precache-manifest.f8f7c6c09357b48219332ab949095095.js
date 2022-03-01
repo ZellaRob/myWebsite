64ead891793bf96335b520e6cebaaa7f114ee3da
HelenRob <helenrob21@gmail.com>
2022-03-01 17:30:05 +0000

    Updates

---
+++ precache-manifest.f8f7c6c09357b48219332ab949095095.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985e1c7bb50c6c43f954beb44e4ea9dd",
+    "url": "/myWebsite/index.html"
+  },
+  {
+    "revision": "d4ca8db3b2faa7b9a9fa",
+    "url": "/myWebsite/static/css/2.0547ff38.chunk.css"
+  },
+  {
+    "revision": "18bd52afe85361dab41e",
+    "url": "/myWebsite/static/css/main.641861f6.chunk.css"
+  },
+  {
+    "revision": "d4ca8db3b2faa7b9a9fa",
+    "url": "/myWebsite/static/js/2.88e12c8d.chunk.js"
+  },
+  {
+    "revision": "fd7076238b7c86d9c0121c33fd23f8b9",
+    "url": "/myWebsite/static/js/2.88e12c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18bd52afe85361dab41e",
+    "url": "/myWebsite/static/js/main.f9531849.chunk.js"
+  },
+  {
+    "revision": "864134502547540d04d6",
+    "url": "/myWebsite/static/js/runtime-main.ba073dbb.js"
+  },
+  {
+    "revision": "b88d95626d003adf4496730bc3460ce7",
+    "url": "/myWebsite/static/media/sunset.b88d9562.JPG"
+  }
+]);
